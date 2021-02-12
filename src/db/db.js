98db3cbaf6d2cc4b1dfb30ebea4a31e383b925b9
DESKTOP-R32AB8C\Comet <sunearth.moon@outlook.com>
2021-02-13 04:33:22 +0800

    add test console

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/audax', {
+mongoose.connect('mongodb://127.0.0.1:27017/audax', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
